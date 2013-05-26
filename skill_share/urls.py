15235118c03c11c58f78f7aff175a4f8a573cacc
Luis Naranjo <luisnaranjo733@gmail.com>
2013-05-26 07:56:32 -0700

    staticed it

--- skill_share/urls.py
+++ skill_share/urls.py
@@ -15,3 +15,5 @@ urlpatterns = patterns('main.views',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('', (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
